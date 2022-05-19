17441d5a571e942b1fe39d2ae64d7382bec48865
ikogarasi <Buttt0n_>
2022-05-20 01:12:53 +0200

    Добавьте файлы проекта.

---
+++ Task1/Task1/Buy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Buy : Product
+    {
+        public Buy(string productName, double productPrice, double productWeight, uint quantity)
+            : base(productName, productPrice, productWeight)
+        {
+            Quantity = quantity;
+            FullPrice = productPrice * quantity;
+            FullWeight = productWeight * quantity;    
+        }
+
+        public uint Quantity { get; private set; }
+        public double FullPrice { get; private set; }
+        public double FullWeight { get; private set; }
+        
+        ~Buy() { }
+    }
+}
